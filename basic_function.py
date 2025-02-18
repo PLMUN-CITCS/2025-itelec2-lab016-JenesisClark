574b17ae61feaaa0cb91c8fe69cf72f886cdf6a3
justine <jg2559973@gmail.com>
2025-02-18 15:02:50 +0800

    Submitting Python Week 04 - Laboratory # 16

---
+++ basic_function.py
@@ -0,0 +1,12 @@
+# basic_function.py
+
+#Define the function
+def greet():
+#Add a docstring
+def greet():
+    """Prints a greeting message."""
+	#Write the function body
+	    print("Hello, Python Students!")
+		#Call the function
+		greet()
+		
